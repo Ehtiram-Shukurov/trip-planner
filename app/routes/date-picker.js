7f227f4f33357d60d02f1d2c83eb6e169f559c4a
Jinming Chen <91038275+JinmingC@users.noreply.github.com>
2024-12-03 18:56:21 -0600

    fix typo

--- app/routes/date-picker.js
+++ app/routes/date-picker.js
@@ -1,7 +1,7 @@
 import Route from '@ember/routing/route';
 
 export default class DatePickerRoute extends Route {
-  beoforeModel() {}
+  beforeModel() {}
 
   model(params) {
     return params.trip_id;
