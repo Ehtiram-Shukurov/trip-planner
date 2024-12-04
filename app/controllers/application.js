e270c4f6a6f0a60ed4e5b65ea4cdecea1e24ec72
William Yang <96196310+DaemonCypher@users.noreply.github.com>
2024-12-04 17:08:12 -0600

    navbar (#20)
    * navbar
    
    * fix navbar

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,18 +1,39 @@
 import Controller from '@ember/controller';
 import { service } from '@ember/service';
 import { action } from '@ember/object';
-import { getOwner } from '@ember/application';
-
-import {
-  signInWithPopup,
-  GoogleAuthProvider,
-  getAuth,
-  signOut,
-  onAuthStateChanged,
-  authStateReady,
-} from 'firebase/auth';
+import { tracked } from '@glimmer/tracking';
 
 export default class ApplicationController extends Controller {
   @service firebase;
   @service auth;
+  @tracked mobile;
+  @tracked isActive= false;
+
+  constructor() {
+    super(...arguments);
+    this.mobile = this.isMobile(); 
+  }
+
+  @action
+  burgerMenue()
+  {
+    if(this.isActive)
+    {
+      this.isActive= false;
+    }
+    else{
+      this.isActive= true;
+    }
+  }
+
+  isMobile()
+  {
+    // code below from https://dev.to/timhuang/a-simple-way-to-detect-if-browser-is-on-a-mobile-device-with-javascript-44j3
+    if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)){
+      return true;
+    }
+    else{
+    return false;
+    }
+  }
 }
