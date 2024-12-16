729350521d596e5478a38e07e4e7e0826b23ef68
Jinming Chen <19jchen2919@gmail.com>
2024-12-16 15:14:55 -0600

    minor fix

--- app/routes/memory/journal.js
+++ app/routes/memory/journal.js
@@ -17,6 +17,7 @@ export default class MemoryJournalRoute extends Route {
     const day = await this.database.getDay(trip_id, date_id);
     this.journal = await this.database.getJournal(trip_id, date_id);
     this.images = await this.database.getImages(trip_id, date_id);
+    console.log("images",this.images);
     return {
       date: new Date(day.date).toLocaleDateString("en-US"),
       date_id: date_id,
